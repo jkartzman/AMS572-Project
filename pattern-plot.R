dd39cfce07d78242609aa67eb42cc8827b0e8087
Joshua Kartzman <71308925+jkartzman@users.noreply.github.com>
2022-12-01 06:22:12 -0500

    Update pattern-plot.R

--- pattern-plot.R
+++ pattern-plot.R
@@ -1,10 +1,10 @@
 library(mice)
-
+# load MCAR/MNAR datasets
 mcarData1 <- read.csv(url("https://raw.githubusercontent.com/jkartzman/AMS572-Project/main/test1-mcar.csv"),header =TRUE)
 mnarData1 <- read.csv(url("https://raw.githubusercontent.com/jkartzman/AMS572-Project/main/test1-mnar.csv"),header =TRUE)
 mcarData2 <- read.csv(url("https://raw.githubusercontent.com/jkartzman/AMS572-Project/main/test2-mcar.csv"),header =TRUE)
 mnarData2 <- read.csv(url("https://raw.githubusercontent.com/jkartzman/AMS572-Project/main/test2-mnar.csv"),header =TRUE)
-
+# get md patterns
 md.pattern(mcarData1[c("X.bu.","X.dm.")])
 md.pattern(mnarData1[c("X.bu.","X.dm.")])
 md.pattern(mcarData2[c("X.hemo.","X.rbcc.","X.class.")])
