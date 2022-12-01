4ee14ff79c5d3fc621fa8a55faf90cb5409bfba9
Joshua Kartzman <71308925+jkartzman@users.noreply.github.com>
2022-12-01 02:25:01 -0500

    Update test2-missing.R

--- test2-missing.R
+++ test2-missing.R
@@ -3,6 +3,7 @@ library(mice)
 
 mcarData <- read.csv(url("https://raw.githubusercontent.com/jkartzman/AMS572-Project/main/test2-mcar.csv"),header =TRUE)
 mnarData <- read.csv(url("https://raw.githubusercontent.com/jkartzman/AMS572-Project/main/test2-mnar.csv"),header = TRUE)
+print("Using mice to impute:")
 imp <- mice(mcarData,m=1,maxit=2,seed=500)
 impMCAR <- complete(imp)
 imp <- mice(mnarData,m=1,maxit=2,seed=500)
@@ -24,3 +25,23 @@ p_value_mnar=1-pchisq(stepAICMNAR$null.deviance-stepAICMNAR$deviance,stepAICMNAR
 p_value_mcar=1-pchisq(stepAICMCAR$null.deviance-stepAICMCAR$deviance,stepAICMCAR$df.null-stepAICMCAR$df.residual)
 print(paste("P-value on imputed MCAR data: ",p_value_mcar))
 print(paste("P-value on imputed MNAR data: ",p_value_mnar))
+
+print("Using listwise deletion:")
+mcar_listwise <- na.omit(mcarData)
+mnar_listwise <- na.omit(mnarData)
+
+mnarModel <- glm(formulaSelectiveFactors, mnar_listwise, family='binomial')
+print(summary(mnarModel))
+mcarModel <- glm(formulaSelectiveFactors, mcar_listwise, family='binomial')
+print(summary(mcarModel))
+
+stepAICMNAR <- stepAIC(mnarModel, direction = "both",trace=FALSE)
+print(summary(stepAICMNAR))
+stepAICMCAR <- stepAIC(mcarModel, direction = "both",trace=FALSE)
+print(summary(stepAICMCAR))
+
+p_value_mnar=1-pchisq(stepAICMNAR$null.deviance-stepAICMNAR$deviance,stepAICMNAR$df.null-stepAICMNAR$df.residual)
+p_value_mcar=1-pchisq(stepAICMCAR$null.deviance-stepAICMCAR$deviance,stepAICMCAR$df.null-stepAICMCAR$df.residual)
+print(paste("P-value on listwise deleted MCAR data: ",p_value_mcar))
+print(paste("P-value on listwise MNAR data: ",p_value_mnar))
+
